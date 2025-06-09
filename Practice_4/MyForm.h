e57d3e73ba940b4a3c89ea6c19072664be23dd8f
Petre_to82@student.itstep.org <Petre_to82@student.itstep.org>
2025-06-09 15:05:47 +0300

    ex2

--- Practice_4/MyForm.h
+++ Practice_4/MyForm.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <cstdlib>
+#include <ctime>
 
 namespace Practice4 {
 
@@ -34,23 +36,27 @@ namespace Practice4 {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Button^  btnGenerateArray;
-	private: System::Windows::Forms::Button^  btnCalculate;
-	private: System::Windows::Forms::TextBox^  txtArraySize;
-	private: System::Windows::Forms::TextBox^  txtResult;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Button^  button3;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::TextBox^  textBox3;
+	private: System::Windows::Forms::TextBox^  textBox4;
+	private: System::Windows::Forms::TextBox^  textBox5;
 	private: System::Windows::Forms::Label^  lblArraySize;
-	private: System::Windows::Forms::Label^  lblResult;
-	private: System::Windows::Forms::ListBox^  lstArray;
-	private: System::Windows::Forms::Label^  lblProduct;
-
-	private: 
-		array<int>^ arrayData;
+	private: System::Windows::Forms::Label^  lblUnsortedArray;
+	private: System::Windows::Forms::Label^  lblSortedArray;
+	private: System::Windows::Forms::Label^  lblSumOdd;
+	private: System::Windows::Forms::Label^  lblCountEven;
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
 		System::ComponentModel::Container ^components;
+		cli::array<int>^ a; 
+		int n, i, j, dop, min, k;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -59,100 +65,152 @@ namespace Practice4 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->btnGenerateArray = (gcnew System::Windows::Forms::Button());
-			this->btnCalculate = (gcnew System::Windows::Forms::Button());
-			this->txtArraySize = (gcnew System::Windows::Forms::TextBox());
-			this->txtResult = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
 			this->lblArraySize = (gcnew System::Windows::Forms::Label());
-			this->lblResult = (gcnew System::Windows::Forms::Label());
-			this->lstArray = (gcnew System::Windows::Forms::ListBox());
-			this->lblProduct = (gcnew System::Windows::Forms::Label());
+			this->lblUnsortedArray = (gcnew System::Windows::Forms::Label());
+			this->lblSortedArray = (gcnew System::Windows::Forms::Label());
+			this->lblSumOdd = (gcnew System::Windows::Forms::Label());
+			this->lblCountEven = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
-			// btnGenerateArray
+			// button1
 			// 
-			this->btnGenerateArray->Location = System::Drawing::Point(12, 41);
-			this->btnGenerateArray->Name = L"btnGenerateArray";
-			this->btnGenerateArray->Size = System::Drawing::Size(129, 23);
-			this->btnGenerateArray->TabIndex = 0;
-			this->btnGenerateArray->Text = L"Згенерувати масив";
-			this->btnGenerateArray->UseVisualStyleBackColor = true;
-			this->btnGenerateArray->Click += gcnew System::EventHandler(this, &MyForm::btnGenerateArray_Click);
+			this->button1->Location = System::Drawing::Point(12, 41);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(129, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Заповнити масив";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
-			// btnCalculate
+			// button2
 			// 
-			this->btnCalculate->Location = System::Drawing::Point(12, 70);
-			this->btnCalculate->Name = L"btnCalculate";
-			this->btnCalculate->Size = System::Drawing::Size(129, 23);
-			this->btnCalculate->TabIndex = 1;
-			this->btnCalculate->Text = L"Обчислити";
-			this->btnCalculate->UseVisualStyleBackColor = true;
-			this->btnCalculate->Click += gcnew System::EventHandler(this, &MyForm::btnCalculate_Click);
+			this->button2->Location = System::Drawing::Point(12, 100);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(129, 23);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Сортувати";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
 			// 
-			// txtArraySize
+			// button3
 			// 
-			this->txtArraySize->Location = System::Drawing::Point(147, 12);
-			this->txtArraySize->Name = L"txtArraySize";
-			this->txtArraySize->Size = System::Drawing::Size(121, 20);
-			this->txtArraySize->TabIndex = 2;
+			this->button3->Location = System::Drawing::Point(12, 152);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(129, 23);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"Обчислити";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
 			// 
-			// txtResult
+			// textBox1
 			// 
-			this->txtResult->Location = System::Drawing::Point(147, 109);
-			this->txtResult->Name = L"txtResult";
-			this->txtResult->ReadOnly = true;
-			this->txtResult->Size = System::Drawing::Size(121, 20);
-			this->txtResult->TabIndex = 3;
+			this->textBox1->Location = System::Drawing::Point(370, 12);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->TabIndex = 3;
+			this->textBox1->Text = L"10";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(147, 70);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(325, 20);
+			this->textBox2->TabIndex = 4;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(147, 126);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(325, 20);
+			this->textBox3->TabIndex = 5;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(370, 152);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(100, 20);
+			this->textBox4->TabIndex = 6;
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(370, 178);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(100, 20);
+			this->textBox5->TabIndex = 7;
 			// 
 			// lblArraySize
 			// 
 			this->lblArraySize->AutoSize = true;
-			this->lblArraySize->Location = System::Drawing::Point(12, 15);
+			this->lblArraySize->Location = System::Drawing::Point(147, 15);
 			this->lblArraySize->Name = L"lblArraySize";
-			this->lblArraySize->Size = System::Drawing::Size(129, 13);
-			this->lblArraySize->TabIndex = 4;
-			this->lblArraySize->Text = L"Введіть розмірність масиву:";
+			this->lblArraySize->Size = System::Drawing::Size(217, 13);
+			this->lblArraySize->TabIndex = 8;
+			this->lblArraySize->Text = L"Введіть кількість елементів масиву (n <= 10):";
 			// 
-			// lblResult
+			// lblUnsortedArray
 			// 
-			this->lblResult->AutoSize = true;
-			this->lblResult->Location = System::Drawing::Point(12, 112);
-			this->lblResult->Name = L"lblResult";
-			this->lblResult->Size = System::Drawing::Size(129, 13);
-			this->lblResult->TabIndex = 5;
-			this->lblResult->Text = L"Добуток парних елементів:";
+			this->lblUnsortedArray->AutoSize = true;
+			this->lblUnsortedArray->Location = System::Drawing::Point(147, 54);
+			this->lblUnsortedArray->Name = L"lblUnsortedArray";
+			this->lblUnsortedArray->Size = System::Drawing::Size(100, 13);
+			this->lblUnsortedArray->TabIndex = 9;
+			this->lblUnsortedArray->Text = L"Заповнений масив:";
 			// 
-			// lstArray
+			// lblSortedArray
 			// 
-			this->lstArray->FormattingEnabled = true;
-			this->lstArray->Location = System::Drawing::Point(288, 12);
-			this->lstArray->Name = L"lstArray";
-			this->lstArray->Size = System::Drawing::Size(184, 173);
-			this->lstArray->TabIndex = 6;
+			this->lblSortedArray->AutoSize = true;
+			this->lblSortedArray->Location = System::Drawing::Point(147, 110);
+			this->lblSortedArray->Name = L"lblSortedArray";
+			this->lblSortedArray->Size = System::Drawing::Size(104, 13);
+			this->lblSortedArray->TabIndex = 10;
+			this->lblSortedArray->Text = L"Сортований масив:";
 			// 
-			// lblProduct
+			// lblSumOdd
 			// 
-			this->lblProduct->AutoSize = true;
-			this->lblProduct->Location = System::Drawing::Point(12, 178);
-			this->lblProduct->Name = L"lblProduct";
-			this->lblProduct->Size = System::Drawing::Size(129, 13);
-			this->lblProduct->TabIndex = 7;
-			this->lblProduct->Text = L"Добуток парних елементів:";
+			this->lblSumOdd->AutoSize = true;
+			this->lblSumOdd->Location = System::Drawing::Point(147, 155);
+			this->lblSumOdd->Name = L"lblSumOdd";
+			this->lblSumOdd->Size = System::Drawing::Size(173, 13);
+			this->lblSumOdd->TabIndex = 11;
+			this->lblSumOdd->Text = L"Сума непарних елементів масиву:";
+			// 
+			// lblCountEven
+			// 
+			this->lblCountEven->AutoSize = true;
+			this->lblCountEven->Location = System::Drawing::Point(147, 181);
+			this->lblCountEven->Name = L"lblCountEven";
+			this->lblCountEven->Size = System::Drawing::Size(175, 13);
+			this->lblCountEven->TabIndex = 12;
+			this->lblCountEven->Text = L"Кількість парних елементів масиву:";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(484, 211);
-			this->Controls->Add(this->lblProduct);
-			this->Controls->Add(this->lstArray);
-			this->Controls->Add(this->lblResult);
+			this->Controls->Add(this->lblCountEven);
+			this->Controls->Add(this->lblSumOdd);
+			this->Controls->Add(this->lblSortedArray);
+			this->Controls->Add(this->lblUnsortedArray);
 			this->Controls->Add(this->lblArraySize);
-			this->Controls->Add(this->txtResult);
-			this->Controls->Add(this->txtArraySize);
-			this->Controls->Add(this->btnCalculate);
-			this->Controls->Add(this->btnGenerateArray);
+			this->Controls->Add(this->textBox5);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
 			this->Name = L"MyForm";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"MyForm";
 			this->ResumeLayout(false);
 			this->PerformLayout();
@@ -160,55 +218,61 @@ namespace Practice4 {
 		}
 #pragma endregion
 
-	private: System::Void btnGenerateArray_Click(System::Object^  sender, System::EventArgs^  e) {
-		try {
-			int size = Convert::ToInt32(txtArraySize->Text);
-			
-			if (size <= 0 || size > 100) {
-				MessageBox::Show("Розмірність масиву повинна бути від 1 до 100!", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
-				return;
-			}
-			
-			// Create and fill the array with random values
-			arrayData = gcnew array<int>(size);
-			Random^ rand = gcnew Random();
-			
-			lstArray->Items->Clear();
-			
-			for (int i = 0; i < size; i++) {
-				arrayData[i] = rand->Next(-50, 51); // Generate numbers from -50 to 50
-				lstArray->Items->Add(i.ToString() + ": " + arrayData[i].ToString());
-			}
-			
-			txtResult->Clear();
-		}
-		catch (Exception^ ex) {
-			MessageBox::Show("Введіть коректне числове значення для розмірності масиву!", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
+	// Заповнити масив
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) 
+	{
+		srand(time(NULL));
+		n = Convert::ToInt32(textBox1->Text);
+		a = gcnew cli::array<int>(n); // Allocate managed array
+		textBox2->Clear();
+		for (i = 0; i < n; i++)
+		{
+			a[i] = rand() % 50;
+			textBox2->AppendText(Convert::ToString(a[i]) + " ");
 		}
 	}
 
-	private: System::Void btnCalculate_Click(System::Object^  sender, System::EventArgs^  e) {
-		if (arrayData == nullptr || arrayData->Length == 0) {
-			MessageBox::Show("Спочатку згенеруйте масив!", "Попередження", MessageBoxButtons::OK, MessageBoxIcon::Warning);
-			return;
-		}
-		
-		int product = 1;
-		bool evenFound = false;
-		
-		for (int i = 0; i < arrayData->Length; i++) {
-			if (arrayData[i] % 2 == 0 && arrayData[i] != 0) { // Check if element is even and not zero
-				product *= arrayData[i];
-				evenFound = true;
+	// Сортувати масив, метод екстремальних елементів
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) 
+	{
+		for (j = 0; j < n-1; j++)
+		{
+			min = j;
+			for (i = j+1; i < n; i++)
+			{
+				if (a[i] > a[min])
+				{
+					dop = a[min];
+					a[min] = a[i];
+					a[i] = dop;
+				}
 			}
 		}
-		
-		if (evenFound) {
-			txtResult->Text = product.ToString();
+		textBox3->Clear();
+		for (i = 0; i < n; i++)
+			textBox3->AppendText(Convert::ToString(a[i]) + " ");
+	}
+
+	// Обчислити суму не парних елементів масиву, кількість парних елементів масиву
+	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) 
+	{
+		k = 0; 
+		int s = 0;
+		textBox4->Clear();
+		for (i = 0; i < n; i++)
+		{
+			if (a[i] % 2 == 1)
+			{
+				s += a[i];
+			}
+			else 
+			{
+				k += 1;
+			}
 		}
-		else {
-			txtResult->Text = "No even elements found";
-		}
+		textBox4->AppendText(Convert::ToString(s));
+		textBox5->Clear();
+		textBox5->AppendText(Convert::ToString(k));
 	}
 	};
 }
